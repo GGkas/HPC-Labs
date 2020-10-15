ff21871286fb8b4e41bea3d3b593af92f3c78cac
GGkas <gkeoof@gmail.com>
2020-10-15 23:55:40 +0300

    Finalised RUN script, next is makefile modifications

--- Lab1/RUN.sh
+++ Lab1/RUN.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-touch log_file.txt
-LOGFILE="log_file.txt"
-
+touch log_file.log
+LOGFILE="log_file.log"
+TEMPFILE="temp.data"
 sh -c "make all"
 
 echo -n "Progress: "
@@ -14,4 +14,14 @@ do
 	./$1 &>> $LOGFILE
 done
 printf '\n'
+
+# Clean up the output of sobel_orig executable for further use
+grep "Total time =" ${LOGFILE} > ${TEMPFILE}
+awk -F'=' '{print $2}' ${TEMPFILE} > times.txt
+
+grep "PSNR" ${LOGFILE} > ${TEMPFILE}
+awk -F':' '{print $2}' ${TEMPFILE} > psnr.txt
+
+rm -f ${LOGFILE} ${TEMPFILE}
+
 sh -c "make clean"
